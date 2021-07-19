24fc7f877d25faad8f3475658fef657174c32864
Alex <alex0692pash@mail.ru>
2021-07-19 22:48:58 +0300

    Init 001

---
+++ 001_Task1/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+/*
+ Напишите функцию swapvec, принимающую std::vector<int> и указатель на массив переменных типа int (одного размера)
+ и обменивающую значения этих массивов.
+Пример:
+std::vector<int> a = {1,2,3,4};
+int b[] = {2,4,6,8};
+swapvec(a,b);
+for(int i = 0; i < 4; ++i)
+   std::cout << a[i];
+std::cout << std::endl;
+for(int i = 0; i < 4; ++i)
+   std::cout << b[i];
+
+Вывод:
+2468
+1234
+ */
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
