ea374189814a1d4a39078dc7438a0d40ca3d79cd
Alex <alex0692pash@mail.ru>
2021-07-20 21:24:45 +0300

    002 done

--- 002_Task2/main.cpp
+++ 002_Task2/main.cpp
@@ -5,14 +5,33 @@
 Кролик хочет допрыгать до n-й ступеньки, но может сделать это большим количеством способов.
 Напишите рекурсивную функцию, которая принимает число n типа int, а возвращает количество способов,
  которыми кролик может доскакать до n-й ступеньки.
-
-Пример:
-Ввод: 3
-Вывод : 4
-Пояснение: (3), (1,2), (2,1), (1,1,1)
  */
 
+bool CheckStair(int a) {
+    return a > 0;
+}
+
+int Count(int a) {
+    if (a < 0) {
+        return 0;
+    } else if (a == 1 || a == 0) {
+        return 1;
+    } else {
+        return Count(a - 1) + Count(a - 2) + Count(a - 3);
+    }
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int n;
+    std::cout << "Input stair number" << std::endl;
+    std::cin >> n;
+
+    if(CheckStair(n)) {
+        std::cout << Count(n) << std::endl;
+    } else {
+        std::cout << "Wrong input" << std::endl;
+    }
+
     return 0;
 }
